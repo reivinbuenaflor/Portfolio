bef5b597a8687bedaf3455217587e80ea38d9abc
reivinbuenaflor <45374752+reivinbuenaflor@users.noreply.github.com>
2019-10-10 21:02:50 +0800

    Update 2dDynamicArray.java

--- 2dDynamicArray.java
+++ 2dDynamicArray.java
@@ -5,52 +5,54 @@ public class Main {
     public static void main(String[] args){
         int[] A;
         int[] B;
-        int[][] C;
+        int[][]C;
         String setRow;
         String setCol;
         Integer getRow;
         Integer getCol;
-        Integer finalResA;
-        Integer finalResB;
-        Integer finalres;
+        Integer finalres = 0;
 
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the length of row: ");
+        System.out.println("Enter the length of First Array: ");
         setRow = sc.next();
         getRow = Integer.parseInt(setRow);
 
-        System.out.println("Enter the length of column: ");
+        System.out.println("Enter the length of Second Array: ");
         setCol = sc.next();
         getCol = Integer.parseInt(setCol);
 
         A = new int[getRow];
         B = new int[getCol];
-
-        System.out.println("*");
+        C = new int[getRow][getCol];
+        
+        System.out.print("  ");
+        for(int i = 1; i < B.length; i++) {
+        	System.out.print(i+"\t\t");
+        }
+       
+        System.out.println("");
+        System.out.print("* ");
+        
+        for(int i = 1; i < B.length; i++) {
+        	System.out.print("*\t\t");
+        }
+        
+        System.out.println(" ");
+        
         for(int i  = 1; i < A.length; i++){
-            System.out.println("*");
-            System.out.print(i+"*");
-            for(int j = 1; j < B.length; j++){
+        	
+        	System.out.print(i+"*");
+           for(int j = 1; j < B.length; j++){
                 A[i] = i;
                 B[j] = j;
-                finalResA = A[i];
-                finalResB = B[j];
-                finalres = finalResA*finalResB;
-                if(finalres < 10) {
-                    System.out.print(finalres+"    ");
-                }
-                else if(finalres< 100) {
-                    System.out.print(finalres+"   ");
-                }else if(finalres< 1000) {
-                    System.out.print(finalres+"  ");
-                }
-                else {
-                    System.out.print(finalres+" ");
-                }
+                C[i][j] = A[i]*B[j];
+                
+                System.out.print(C[i][j]+"\t\t");
             }
-            System.out.println(" ");
-        }
-    }
+           System.out.println("");
+       }
+	
+	}
 }
