40f83fbf1ca3a967f2f2f5497da9664e976c7663
reivinbuenaflor <45374752+reivinbuenaflor@users.noreply.github.com>
2019-10-11 07:53:01 +0800

    2D_DynamicArray

--- 2dDynamicArray.java
+++ 2dDynamicArray.java
@@ -2,57 +2,54 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] A;
         int[] B;
         int[][]C;
-        String setRow;
-        String setCol;
-        Integer getRow;
-        Integer getCol;
+        
+        Integer getfArray;
+        Integer getsArray;
         Integer finalres = 0;
 
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the length of First Array: ");
-        setRow = sc.next();
-        getRow = Integer.parseInt(setRow);
-
-        System.out.println("Enter the length of Second Array: ");
-        setCol = sc.next();
-        getCol = Integer.parseInt(setCol);
-
-        A = new int[getRow];
-        B = new int[getCol];
-        C = new int[getRow][getCol];
+        getfArray = sc.nextInt();
         
+        System.out.println("Enter the length of Second Array: ");
+        getsArray = sc.nextInt();
+        
+        A = new int[getfArray];
+        B = new int[getsArray];
+        C = new int[getfArray][getsArray];
+        
+        System.out.println("");
         System.out.print("  ");
         for(int i = 1; i < B.length; i++) {
-        	System.out.print(i+"\t\t");
+            System.out.print(i+"\t\t");
         }
-       
+
         System.out.println("");
         System.out.print("* ");
-        
+
         for(int i = 1; i < B.length; i++) {
-        	System.out.print("*\t\t");
+            System.out.print("*\t\t");
         }
-        
+
         System.out.println(" ");
-        
+
         for(int i  = 1; i < A.length; i++){
-        	
-        	System.out.print(i+"*");
-           for(int j = 1; j < B.length; j++){
+
+            System.out.print(i+"*");
+            for(int j = 1; j < B.length; j++){
                 A[i] = i;
                 B[j] = j;
                 C[i][j] = A[i]*B[j];
-                
+
                 System.out.print(C[i][j]+"\t\t");
             }
-           System.out.println("");
-       }
-	
+            System.out.println("");
+        }
     }
 }
